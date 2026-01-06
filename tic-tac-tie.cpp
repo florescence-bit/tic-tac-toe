0d67f9811241daa13f533decc5d1c61e78fcb762
san <adityarajsharma.021@gamil.com>
2026-01-06 17:49:56 +0530

    board with symbols is done (draw();)

--- tic-tac-tie.cpp
+++ tic-tac-tie.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
 using namespace std;
 
+class Tic {
+    private:
+    int mat [3][3] = {{0,1,2},
+	   	      {0,1,2},
+		      {0,1,2}};
+
+    public:
+    Tic () {	
+        draw();
+    }
+    private:
+    void draw () {
+	for(int j = 0; j < 3; j++) {
+	    for(int i = 0; i < 3; i++) {
+		if (mat[j][i]==0) cout << "  ";
+		if (mat[j][i]==1) cout << " ○";
+		if (mat[j][i]==2) cout << " ×";
+	    }
+	    cout << endl;
+	}
+    }
+};
+
 int main () {
-    cout << "working..." << endl;	
+    Tic T1 = Tic();
     return 0;
 }
