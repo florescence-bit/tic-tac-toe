4ec25e210a88491c241d2c0b62b0958adf43b83c
san <adityarajsharma.021@gamil.com>
2026-01-07 08:25:53 +0530

    result function half ready working on it

--- tic-tac-tie.cpp
+++ tic-tac-tie.cpp
@@ -3,23 +3,49 @@ using namespace std;
 
 class Tic {
     private:
-    int mat [3][3] = {{0,1,2},
-	   	      {0,1,2},
-		      {0,1,2}};
+    int mat [9] = {0,1,2,
+	    	   0,1,2,
+		   0,1,2};
+    int win [8] = {{0,1,2},
+	    	  {3,4,5},
+		  {6,7,8},
+		  {0,3,6},
+		  {1,4,7},
+		  {2,5,8},
+		  {0,4,8},
+		  {2,4,6}};
+    int moves = 9;
 
     public:
-    Tic () {	
+    Tic () {
+	cout << "\e[2J\e[H";
         draw();
     }
     private:
     void draw () {
 	for(int j = 0; j < 3; j++) {
 	    for(int i = 0; i < 3; i++) {
-		if (mat[j][i]==0) cout << "  ";
-		if (mat[j][i]==1) cout << " ○";
-		if (mat[j][i]==2) cout << " ×";
+		int loc = mat[j*3+i];
+		if (loc) cout << " □";
+		if (loc) cout << " ○";
+		if (loc) cout << " ×";
 	    }
 	    cout << endl;
+	}
+    }
+    private:
+    void action () {
+        if(moves == 0) result();
+    }
+    private:
+    void result () {
+        for (int j = 0; j < 12; j++) {
+		bool isMatch = true;
+	    for (int i = 0; i < 3; i++) {
+		if (mat[win[j][i]]) break;
+		int firstN = win[j][0];
+	        mat[i] 
+	    }
 	}
     }
 };
